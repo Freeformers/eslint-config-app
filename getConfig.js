9a1c15793efd4153744b0a35c7facc754d822e3f
Anthony Wilkinson <wilkinson.anthonyj@gmail.com>
2021-04-26 12:44:00 +0100

    Update simple import sort

--- getConfig.js
+++ getConfig.js
@@ -54,7 +54,7 @@ module.exports = {
           object: true
         }
       ],
-      "simple-import-sort/sort": "error",
+      "simple-import-sort/import": "error",
       "@typescript-eslint/no-empty-interface": "off",
       camelcase: "off",
       "@typescript-eslint/camelcase": [
